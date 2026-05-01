55e1c00010d9f99cedf4214fbcc8339510c1661e
Jaamal Oakmon <Jaamal.Oakmon@gmail.com>
2026-05-01 18:02:41 -0500

    Fix Firebase auth initialization

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,6 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getReactNativePersistence, initializeAuth } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCe1klvB0PcMJ9nFS-N6NaMYoqDAhVIcew",
@@ -7,9 +8,22 @@ const firebaseConfig = {
   projectId: "streamfusion-5aea2",
   storageBucket: "streamfusion-5aea2.firebasestorage.app",
   messagingSenderId: "439873605921",
-  appId: "1:439873605921:web:ded29edb3665f216712e60",
-  measurementId: "G-999DFR72X2"
+  appId: "1:439873605921:web:ded29edb3665f216712e60"
 };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+let app;
+let auth;
+
+if (getApps().length === 0) {
+  app = initializeApp(firebaseConfig);
+  auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage)
+  });
+} else {
+  app = getApp();
+  auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage)
+  });
+}
+
+export { auth };
